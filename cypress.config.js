1c3e990ec5e8c9f55dde5e6c7d8a6536e5be3e5c
Nada Eladawy <nm171471@gmail.com>
2025-02-24 23:50:12 +0200

    edit in redme file commit

--- cypress.config.js
+++ cypress.config.js
@@ -6,12 +6,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // specPattern: "Automation-Task-main\Automation-Task\cypress\e2e\productCheckout\productCheckout.cy.js",
   },
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
 });
